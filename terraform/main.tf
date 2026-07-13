ebe8dfb67de9e84fa21e40bd5b69c6a1718f15af
Mohamed Nada <mohammed.nada11277@gmail.com>
2026-07-13 15:29:32 +0300

    feat(security): add security groups module

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,3 +10,30 @@ module "network" {
   private_app_availability_zone = var.private_app_availability_zone
   private_db_availability_zone  = var.private_db_availability_zone
 }
+
+
+
+module "security" {
+  source = "./modules/security"
+
+  project_name = var.project_name
+  vpc_id       = module.network.vpc_id
+}
+
+
+
+module "compute"{
+
+  source = "./modules/compute"
+
+  public_subnet_id = module.network.public_subnet_id
+  private_app_subnet_id = module.network.private_app_subnet_id
+  private_db_subnet_id = module.network.private_db_subnet_id
+
+
+  frontend_sg_id = module.security.frontend_sg_id
+  backend_sg_id = module.security.backend_sg_id
+  db_sg_id = module.security.db_sg_id
+  
+
+}
