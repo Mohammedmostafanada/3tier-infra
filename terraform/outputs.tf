ecbc85869cb645a362d68548e5bfe8c1f347c01b
Mohamed Nada <mohammed.nada11277@gmail.com>
2026-07-17 03:08:26 +0300

    feat(terraform): expose compute module outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -13,3 +13,18 @@ output "private_app_subnet_id" {
 output "private_db_subnet_id" {
   value = module.network.private_db_subnet_id
 }
+
+output "frontend_public_ip" {
+  description = "The public IP address of the frontend server"
+  value       = module.compute.frontend_public_ip
+}
+
+output "backend_private_ip" {
+  description = "The private IP address of the backend server"
+  value       = module.compute.backend_private_ip
+}
+
+output "database_private_ip" {
+  description = "The private IP address of the database server"
+  value       = module.compute.database_private_ip
+}
